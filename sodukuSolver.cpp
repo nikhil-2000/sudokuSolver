696e070494fcb7534db4480d67d4c06d2c80cbee
Nikhil <nikhil_2000@bitbucket.org>
2020-04-20 21:53:03 +0100

    Can read soduku's in and fills the grid

--- sodukuSolver.cpp
+++ sodukuSolver.cpp
@@ -1,51 +1,123 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <tuple>
+#include <string>
+#include <fstream>
 
 class Grid {
   public:
     int grid[9][9];
+    std::vector< std::tuple<int,int> > gaps;
 
-    void fillGrid(){
-      for (int i = 0;  i < 9; i++){
-        for (int j = 0;  j < 9; j++ ){
-          grid[i][j] = 0;
-        }
-      }
+    void main() {
+      //Input file name
+      fillGrid("unsolved soduku.txt");
+      fillGaps();
+
     }
 
     void outputGrid(){
       bool bottomLine = false;
+    
       for (int i = 0;  i < 9; i++){
+
         if (i % 3 == 2){
           bottomLine = true;
         }
+
         for (int j = 0;  j < 9; j++ ){
-          cout << grid[i][j];
+
+          std::cout << grid[i][j];
+
           if (j % 3 == 2 && j != 8) {
-            cout << "|";
+            std::cout << "|";
           }
+
         }
         
-        cout << '\n';
+        std::cout << '\n';
+
         if (bottomLine && i != 8){
+
           for (int j = 0;  j < 11; j++ ){
+
             if (j % 4 == 3) {
-              cout << "+";
+              std::cout << "+";
             }else{
-              cout << "-";
+              std::cout << "-";
             }
           }
-          cout << '\n';
+
+          std::cout << '\n';
           bottomLine = false;
+
         }
       }
     }
+
+    void outputGaps(){
+      for (std::vector<int>::size_type i = 0; i < gaps.size(); i++) {
+	      outputTuple(gaps.at(i));
+      }
+    }
+
+  private:
+    void fillGrid(std::string filename){
+      std::ifstream inFile;
+      std::string oneLineSoduku;
+      std::string num;
+      
+      inFile.open(filename);
+
+      while (inFile >> num){
+        oneLineSoduku.append(num);
+      }
+
+      int i = 0;
+      int x,y;
+      for (char& n : oneLineSoduku){
+        x = (i) / 9;
+        y = (i) % 9;
+        grid[x][y] = n - '0';
+        i++;
+      }
+
+
+      inFile.close();
+    }
+
+    bool isGap(int i,int j) {
+      return (grid[i][j] == 0);
+    }
+
+    void outputTuple(std::tuple <int , int> t){
+
+      std::cout << std::get<0>(t) << std::get<1>(t) << ' ' << '\n'; 
+
+    }
+    
+    void fillGaps(){
+      std::tuple<int,int> loc;
+
+      for (int i = 0;  i < 9; i++){
+
+        for (int j = 0;  j < 9; j++ ){
+
+          if (isGap(i,j)){
+
+            loc = {i,j};
+            gaps.insert(gaps.end(),loc);
+          }
+        }
+      }
+    }
+
 };
 
 
 int main() {
   Grid sodukuGrid;
-  sodukuGrid.fillGrid();
+  sodukuGrid.main();
   sodukuGrid.outputGrid();
   return 0;
 }
