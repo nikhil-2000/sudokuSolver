522bfef61a83161628766eabd5a73a772add61fa
Nikhil <nikhil_2000@bitbucket.org>
2020-05-25 20:51:05 +0100

    Changed readImage.py to read in sudoku's with no whitespace around the border Also, tried to handle case where sudoku's are coloured(needs to be tested)

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ import subprocess
 def main():
     filename = sys.argv[1]
     oneLineSoduku = readImage.main(filename)
-    print(oneLineSoduku)
-
-    print(subprocess.check_output(['./sodukuSolver', '030007004000195000008000060820062004000800001000020000060002280000019005000000070']))
-
+    args = ['./sodukuSolver', oneLineSoduku]
+    p = subprocess.Popen(args)
+    print(p)
 
 if __name__ == "__main__":
     main()    
--- readImage.py
+++ readImage.py
@@ -3,34 +3,89 @@ import sys
 import pytesseract
 import numpy as np
 
+def checkHorizontalBlackLine(image,currentY,dimensions):
+	h,w = dimensions
+
+
+
+	xPoints = np.arange(0, w)
+	colours = {'black': 0 , 'other': 0}
+	for x in xPoints:
+		# x,y = int(x),int(y)
+		pixel = image[currentY,x]
+
+		if pixel == 0:
+			colours['black'] += 1
+		else:
+			colours['other'] += 1
+	
+
+	return colours['black'] > colours['other']
+
+def checkVerticalBlackLine(image,currentX,dimensions):
+	h,w = dimensions
+
+
+	yPoints = np.arange(0, h)
+	colours = {'black': 0 , 'other': 0}
+	for y in yPoints:
+		# x,y = int(x),int(y)
+		pixel = image[y,currentX]
+
+		if pixel == 0:
+			colours['black'] += 1
+		else:
+			colours['other'] += 1
+	
+
+	return colours['black'] > colours['other']
 
 def cropImage(image):
-	w,h,c = image.shape
-	xmin,xmax,ymin,ymax = w,0,h,0
+	h,w = image.shape[0]-1,image.shape[1]-1
+	
+	xmin,xmax,ymin,ymax = 0,w,0,h
 
-	for x in range(w):
-		for y in range(h):
-			if np.all(image[x,y] == 0):
-				if x < xmin : xmin = x
-				if y < ymin : ymin = y
-				if x > xmax : xmax = x
-				if y > ymax : ymax = y
+	
+	currentY = ymin
+	while not checkHorizontalBlackLine(image,currentY,(h,w)) and currentY < h:
+		currentY += 1
 
-	croppedWidth = xmax - xmin + (9 - (xmax - xmin) % 9)
-	croppedHeight = ymax - ymin + (9 - (ymax - ymin) % 9)
+	ymin = currentY
+	
+	currentY = ymax
+	while not checkHorizontalBlackLine(image,currentY,(h,w)) and currentY > 0:
+		currentY -= 1
+
+	ymax = currentY
+
+	currentX = xmin
+	while not checkVerticalBlackLine(image,currentX,(h,w)) and currentX < w:
+		currentX += 1
+	
+	xmin = currentX
+
+	currentX = xmax
+	while not checkVerticalBlackLine(image,currentX,(h,w)) and currentX > 0:
+		currentX -= 1
+	
+	xmax = currentX
+
+
+	croppedWidth = xmax - xmin
+	croppedHeight = ymax - ymin
+
 
 	crop_img = image[ymin:ymin + croppedHeight , xmin:xmin + croppedWidth]
-
 	return crop_img
 
 
 def split_sudoku_cells(img):
 	cells = []
 	
-	rows = np.split(img, 9)
+	rows = np.array_split(img, 9)
 
 	for r in rows:
-		splitRow = np.split(r,9,axis = 1)
+		splitRow = np.array_split(r,9,axis = 1)
 		for p in splitRow:
 			cells.append(p)
 	
@@ -62,19 +117,28 @@ def extractDigits(cells):
 	return soduku
 
 def getOneLineSoduku(filename):
-	img = cv2.imread(filename)
-	
+	img = cv2.imread("testImage.jpg")
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+	(thresh, img) = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+	cv2.imshow("Disp",img)
+	cv2.waitKey(3000)
 	img = cropImage(img)
 
 
-
 	cells = split_sudoku_cells(img)
-
 	oneLineSoduku = extractDigits(cells)
+	
+	cv2.imshow("Disp 2",img)
+	cv2.waitKey(5000)
 	return oneLineSoduku
-
-def main(filename):	
-	return (getOneLineSoduku(filename))
+	
+# def main(filename):
+def main():	
+	
+	print(getOneLineSoduku(""))
 
 
 if __name__ == "__main__":
